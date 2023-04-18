ea1fe9e78f970a3f6f024d8daaf9fccc09edc1a0
Kamil <KamilWlodarczyk2003@gmail.com>
2023-04-18 15:57:41 +0200

    added ASCII art in art.py file, fixed the bug when user enter number greater than 26, added option to put symbols, possibility of using function multiple time without restarting program, added everything into one funtion

--- Ceasar_Cipher.py
+++ Ceasar_Cipher.py
@@ -1,19 +1,29 @@
 ﻿from alphabet import alphabet
+from art import logo
+
+print(logo)
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def Ceasar():
+    question ="yes"
+    while question=="yes":
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))%26
+        ceasar_text=""
+        if(direction=="encode"):
+            for x in text:
+                if x in alphabet:
+                    ceasar_text+=alphabet[alphabet.index(x)+shift]
+                else:
+                    ceasar_text+=x
+        if(direction=="decode"):
+            for x in text:
+                if x in alphabet:
+                    ceasar_text+=alphabet[alphabet.index(x)-shift]
+                else:
+                    ceasar_text+=x
+        print(ceasar_text)
+        question=input('If you want to continue type "yes", if not type anything else\n')
 
-
-def Ceasar(direction, text, shift):
-    ceasar_text=""
-    if(direction=="encode"):
-        for x in text:
-            ceasar_text+=alphabet[alphabet.index(x)+shift]
-    if(direction=="decode"):
-        for x in text:
-            ceasar_text+=alphabet[alphabet.index(x)-shift]
-    print(ceasar_text)
-
-Ceasar(direction,text,shift)
+Ceasar()
