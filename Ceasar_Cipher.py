c4bba4b7a8a9b022dbf587e2174f0bce6c98bfe8
Kamil <KamilWlodarczyk2003@gmail.com>
2023-04-18 15:23:29 +0200

    Added decryption function

--- Ceasar_Cipher.py
+++ Ceasar_Cipher.py
@@ -6,11 +6,21 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-print(text[0])
 def encrypt(text, shift):
     cipher_text=""
     for x in text:
         cipher_text+=alphabet[alphabet.index(x)+shift]
     print(cipher_text)
 
-encrypt(text, shift)
+def decrypt(cipher, shift):
+    fixed_text=""
+    for x in cipher:
+        fixed_text+=alphabet[alphabet.index(x)-shift]
+    print(fixed_text)
+
+if direction=="encode":
+    encrypt(text,shift)
+elif direction=="decode":
+    decrypt(text,shift)
+else:
+    print("Wrong input")
