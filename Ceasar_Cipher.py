09ab970cd07c9b8137291ecd6537d37d981c69bb
Kamil <KamilWlodarczyk2003@gmail.com>
2023-04-18 15:32:19 +0200

    Combined all the function into one

--- Ceasar_Cipher.py
+++ Ceasar_Cipher.py
@@ -6,21 +6,14 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
-    cipher_text=""
-    for x in text:
-        cipher_text+=alphabet[alphabet.index(x)+shift]
-    print(cipher_text)
+def Ceasar(direction, text, shift):
+    ceasar_text=""
+    if(direction=="encode"):
+        for x in text:
+            ceasar_text+=alphabet[alphabet.index(x)+shift]
+    if(direction=="decode"):
+        for x in text:
+            ceasar_text+=alphabet[alphabet.index(x)-shift]
+    print(ceasar_text)
 
-def decrypt(cipher, shift):
-    fixed_text=""
-    for x in cipher:
-        fixed_text+=alphabet[alphabet.index(x)-shift]
-    print(fixed_text)
-
-if direction=="encode":
-    encrypt(text,shift)
-elif direction=="decode":
-    decrypt(text,shift)
-else:
-    print("Wrong input")
+Ceasar(direction,text,shift)
